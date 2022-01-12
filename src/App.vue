4c841def8fcad15337235b892d3029de9ceb7b92
siggi <siegfried.hamm@gmail.com>
2022-01-12 12:10:37 +0100

    especially cool

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <h1>Binding Classes</h1>
+    <h2 class="underline" v-bind:class="state">Status</h2>
+    <h3 v-bind:class="{ especially: 'IsEspecially' }">
+      Ist dies ein besonderer Text
+    </h3>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      state: "success",
+      isEspecially: false,
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.underline {
+  text-decoration: underline;
+}
+.especially {
+  font-weight: 800;
+  font-style: italic;
+}
 </style>
