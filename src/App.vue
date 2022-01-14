0db8661819002b52509fda04475c495b1b96421a
siggi <siegfried.hamm@gmail.com>
2022-01-14 09:26:30 +0100

    divSize

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <div class="main">
-    <h1>Cigarettes</h1>
-    <h2 class="underline" v-bind:class="state">Status</h2>
-    <h3 v-bind:class="active ? 'especially' : 'normal'">
-      Ist dies ein besonderer Text
-    </h3>
-    <h4
-      v-bind:class="{
-        underline: true,
-        especially_bold: active && IsBold,
-        especially: active && !IsBold,
-        only_bold: !IsEspecially && IsBold,
-      }"
-    >
-      Ein Array mit mehreren Klassen
-    </h4>
+    <h1>Binding Styles</h1>
     <div
       v-bind:style="{
         color: highlightColor,
+        'font-size': divSize + 'px',
       }"
     >
       Inline Stlye
@@ -30,10 +17,8 @@ export default {
   name: "App",
   data() {
     return {
-      state: "success",
-      active: true,
-      isBold: true,
       highlightColor: "#4FC08D",
+      divSize: "50",
     };
   },
 };
@@ -41,27 +26,10 @@ export default {
 
 <style scoped>
 .main {
-  background-color: #000;
-  color: white;
   padding: 10px;
   font-family: Georgia, "Times New Roman", Times, serif;
 }
 .underline {
   text-decoration: underline;
-}
-.especially {
-  font-weight: 800;
-  font-style: italic;
-}
-.especially_bold {
-  font-weight: 800;
-  color: orange;
-  font-style: italic;
-}
-.only_bold {
-  font-weight: 800;
-}
-.normal {
-  color: aqua;
 }
 </style>
