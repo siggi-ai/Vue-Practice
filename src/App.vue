746c50f1658073ce4893316f6d5adcae38b373af
siggi <siegfried.hamm@gmail.com>
2022-01-17 15:58:28 +0100

    key and item

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="main">
     <h1>List Rendering</h1>
-    <template v-for="(fullName, index) in fullNames" :key="index">
-      <h2>
-        {{ fullName.name }}, {{ fullName.description }}, {{ fullName.age }}
-      </h2>
-      <h4 v-for="(character, index) in fullName.characters" :key="index">
-        {{ character }}
-      </h4>
-      <br />
+    <template v-for="(item, key) in fullNames" :key="key">
+      <h3>{{ key }}: {{ item }}</h3>
     </template>
   </div>
 </template>
@@ -18,31 +12,11 @@ export default {
   name: "App",
   data() {
     return {
-      fullNames: [
-        {
-          name: "Hans",
-          description: "Engineer",
-          age: 32,
-          characters: ["cool", "reliable", "fancy"],
-        },
-        {
-          name: "Gabi",
-          description: "Politician",
-          age: 42,
-          characters: ["eloquent", "intelligent", "greedy"],
-        },
-        {
-          name: "Melanie",
-          description: "Author",
-          age: 29,
-          characters: ["playful", "creative", "unreliable"],
-        },
-      ],
-      fullPlaces: [
-        { name: "Hamburg", description: "Northern Germany" },
-        { name: "Berlin", description: "East Germany" },
-        { name: "Munich", description: "South Germany" },
-      ],
+      fullNames: {
+        name: "Franz",
+        description: "Engineer",
+        age: 32,
+      },
     };
   },
 };
