18d79c02a5f59c3a3ad0660a870b2767312d66aa
siggi <siegfried.hamm@gmail.com>
2022-01-18 19:54:31 +0100

    conditional list rendering

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div class="main">
-    <h1>Lists and Keys</h1>
-    <div v-for="name in names" :key="name">
-      <h3>{{ name }}</h3>
-      <input placeholder="Nachname" />
-    </div>
-    <button class="button" v-on:click="changeOrder">Liste neu sortieren</button>
+    <h1>Conditional List Rendering</h1>
+
+    <template v-for="name in names" :key="name">
+      <h3 v-if="name === 'Annalena'">{{ name }}</h3>
+    </template>
   </div>
 </template>
 
 <script>
-import _ from "lodash";
 export default {
   name: "App",
   data() {
     return {
       names: ["Angela", "Armin", "Annalena", "Olaf"],
     };
-  },
-  methods: {
-    changeOrder() {
-      console.log(this.names);
-      this.names = _.shuffle(this.names);
-    },
   },
 };
 </script>
