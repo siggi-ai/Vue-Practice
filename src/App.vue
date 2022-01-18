a26a7c943f455298f55f8d8183eff8f622f18afe
siggi <siegfried.hamm@gmail.com>
2022-01-18 20:06:33 +0100

    adding with parameters

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="main">
     <h1>Methods</h1>
-    <h4>Summe von 1 + 2 + 3 + 4 + 5 + 6 = {{ add() }}</h4>
+    <h4>Summe von 1 + 2 + 3 + 4 + 5 + 6 = {{ add(1, 2, 3, 4, 5, 6) }}</h4>
+    <h4>Summe von 1 + 2 + 3 + 5 + 8 + 13 = {{ add(1, 2, 3, 5, 8, 13) }}</h4>
   </div>
 </template>
 
@@ -12,8 +13,8 @@ export default {
     return {};
   },
   methods: {
-    add() {
-      return 1 + 2 + 3 + 4 + 5 + 6;
+    add(a, b, c, d, e, f,) {
+      return a + b + c + d + e + f;
     },
   },
 };
