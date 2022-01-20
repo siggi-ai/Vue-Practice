f18364cb704926aa87a17618a02116fbd304f531
siggi <siegfried.hamm@gmail.com>
2022-01-20 11:23:44 +0100

    counter -5 / +5

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
     <h4>Event Handling</h4>
     <h1>{{ count }}</h1>
     <div>
-      <button v-on:click="changeCount(1)">Plus</button>
-      <button v-on:click="changeCount(-1)">Minus</button>
+      <button v-on:click="changeCount(1)">Plus 1</button>
+      <button v-on:click="changeCount(5)">Plus 5</button>
+      <button v-on:click="changeCount(-1)">Minus 1</button>
+      <button v-on:click="changeCount(-5)">Minus 5</button>
     </div>
   </div>
 </template>
