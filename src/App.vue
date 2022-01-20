6b4c927a149a3d8bc58e51697cd4367ec371e0ce
siggi <siegfried.hamm@gmail.com>
2022-01-20 11:36:26 +0100

    event

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <h4>Event Handling</h4>
     <h1>{{ count }}</h1>
     <div>
-      <button v-on:click="changeCount(1)">Plus 1</button>
-      <button v-on:click="changeCount(5)">Plus 5</button>
-      <button v-on:click="changeCount(-1)">Minus 1</button>
-      <button v-on:click="changeCount(-5)">Minus 5</button>
+      <button v-on:click="changeCount(1, $event)">Plus 1</button>
+      <button v-on:click="changeCount(5, $event)">Plus 5</button>
+      <button v-on:click="changeCount(-1, $event)">Minus 1</button>
+      <button v-on:click="changeCount(-5, $event)">Minus 5</button>
     </div>
   </div>
 </template>
@@ -20,7 +20,8 @@ export default {
     };
   },
   methods: {
-    changeCount(value) {
+    changeCount(value, event) {
+      console.log("event: ", event)
       this.count = this.count + value;
     },
   },
