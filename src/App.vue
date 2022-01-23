531def5adc522741f1a909fc005359ea2a5720a1
siggi <siegfried.hamm@gmail.com>
2022-01-23 10:39:25 +0100

    building the form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="main">
-    <h1>Form Handling</h1>
-    <label class="label" for="firstName">
-      <span class="label-text">Vorname</span>
-    </label>
-    <input 
-      type="text"
-      id="firstname"
-      v-model="form.firstName"
-      placeholder="Vorname"
-      class="input input-bordered">
+    <h2>Eingabebereich</h2>
+    <form action="">
+      <div>
+        <h1>Form Handling</h1>
+        <label class="label" for="firstName">
+          <span class="label-text">Vorname</span>
+        </label>
+        <input
+          type="text"
+          id="firstname"
+          v-model="form.firstName"
+          placeholder="Vorname"
+          class="input input-bordered"
+        />
+        <div>
+          <label class="label" for="lastName">
+            <span class="label-text">Nachname</span>
+          </label>
+          <input
+            type="text"
+            id="lastname"
+            v-model="form.lastName"
+            placeholder="Nachname"
+            class="input input-bordered"
+          />
+        </div>
+      </div>
+    </form>
+    <h2>Ausgabebereich</h2>
+    <div>
+      <div v-for="(item, key, index) in form" :key="index">
+        {{ key }}: {{ item }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,21 +41,9 @@
 export default {
   name: "App",
   data() {
-    return {
-      count: 0,
-      name: "Hanni",
-    };
+    return {};
   },
-  methods: {
-    changeCount(value, event) {
-      console.log("event: ", event)
-      this.count = this.count + value;
-    },
-    changeName(event) {
-      console.log("changeName: ", event);
-      this.name = "Bernd";
-    },
-  },
+  methods: {},
 };
 </script>
 
