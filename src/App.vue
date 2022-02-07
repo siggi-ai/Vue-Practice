8b5597275ccc85a5cbfa1c5b96d59acde1e0ead7
siggi <siegfried.hamm@gmail.com>
2022-02-07 17:02:15 +0100

    new

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,6 @@
 <template>
-  <div class="text">
-    <h1>Computed Properties</h1>
-    <template v-for="players in players" :key="players.id">
-      <div v-if="players.numberOfMatches > 500">
-        {{ players.firstName }} {{ players.lastName }} {{ players.club }} hat
-        insgesamt {{ players.numberOfMatches }} Bundesligaspiele absolviert.
-      </div>
-      <br />
-    </template>
-    <br />
-    <button
-      v-on:click="
-        players.push({
-          id: 4,
-          firstName: 'Hans',
-          lastName: 'Wurst',
-          club: 'FC Schalke 04',
-          numberOfMatches: 189,
-        })
-      "
-    >
-      Mit Hannes
-    </button>
-    <h4>
-      Bundesligaspiele Summe:
-      {{ sumNumberOfMatches }}
-    </h4>
-    <h4>
-      Bundesligaspiele Summe (methods):
-      {{ getSumNumberOfMatches() }}
-    </h4>
-    <div>
-      <label class="label">
-        <span class="label-text" for="color">Farbe</span>
-      </label>
-      <input type="text" id="color" v-model="color" class="input" />
-    </div>
+  <div class="main">
+    <h1>computed properties</h1>
   </div>
 </template>
 
@@ -45,54 +10,13 @@ export default {
   data() {
     return {
       color: "",
-      players: [
-        {
-          id: 1,
-          firstName: "Klaus",
-          lastName: "Fischer",
-          club: "FC Schalke 04",
-          numberOfMatches: 535,
-        },
-        {
-          id: 2,
-          firstName: "Norbert",
-          lastName: "Nigbur",
-          club: "FC Schalke 04",
-          numberOfMatches: 456,
-        },
-        {
-          id: 3,
-          firstName: "Norbert",
-          lastName: "Fichtel",
-          club: "FC Schalke 04",
-          numberOfMatches: 552,
-        },
-      ],
     };
-  },
-  methods: {
-    getSumNumberOfMatches() {
-      console.log("methods getSumNumberOfMatches");
-      return this.players.reduce(
-        (total, current) => (total = total + current.numberOfMatches),
-        0
-      );
-    },
-  },
-  computed: {
-    sumNumberOfMatches() {
-      console.log("computed sumNumberOfMatches");
-      return this.players.reduce(
-        (total, current) => (total = total + current.numberOfMatches),
-        0
-      );
-    },
   },
 };
 </script>
 
 <style scoped>
-.text {
+.main {
   padding: 50px;
   font-family: Georgia, "Times New Roman", Times, serif;
 }
