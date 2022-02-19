a61c0e0b33145a81b2dd02aafc42ebb90655b01d
siggi <siegfried.hamm@gmail.com>
2022-02-19 11:13:53 +0100

    new task

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,12 @@
 <template>
   <div class="main">
-    <h1>Watchers</h1>
-    <h2>Immediate and Deep</h2>
-    <div>
-      <span>Radiosender</span>
-      <input type="text" v-model="radiosender" />
-    </div>
-    <div>
-      <span>Webadresse:</span>
-      <input type="text" v-model="senderInfo.web" />
-    </div>
-    <div>
-      <span>gegründet am:</span>
-      <input type="text" v-model="senderInfo.foundedOn" />
-    </div>
+    <h1>first child</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      radiosender: "Delta Radio",
-      senderInfo: {
-        web: "https://www.deltaradio.de/",
-        foundedOn: 1993,
-      },
-    };
-  },
-  watch: {
-    radiosender: {
-      handler(newSender) {
-        console.log(`Aufruf der API mit dem neuen Radiosender: ${newSender}`);
-      },
-      immediate: true,
-    },
-    senderInfo: {
-      handler(newInfos) {
-        console.log(
-          `Aufruf der API mit web "${newInfos.web}" und gegründet am "${newInfos.foundedOn}`
-        );
-      },
-      deep: true,
-    },
-  },
 };
 </script>
 
