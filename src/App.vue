60c3c9c0a7c36ee118ac5a2da1bfe1166edbb6f9
siggi <siegfried.hamm@gmail.com>
2022-02-20 17:38:34 +0100

    v-bind spectators

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <Spielbericht
       home-team="FC Schalke 04"
       guest-team="Borussia Dortmund"
-      spectators="70 000"
+      v-bind:spectators="70000"
       is-published="true"
     />
 
