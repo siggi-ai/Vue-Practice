e020a96397b5fb6ba773c38f9fdd67a206977269
siggi <siegfried.hamm@gmail.com>
2022-02-20 17:06:58 +0100

    data ht gt

--- src/components/Spielbericht.vue
+++ src/components/Spielbericht.vue
@@ -4,3 +4,12 @@
         <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
     </div>
 </template>
+<script>
+export default {
+    name: "Spielbericht",
+    props: {
+        homeTeam {},
+        guestTeam {},
+    }
+}
+</script>
