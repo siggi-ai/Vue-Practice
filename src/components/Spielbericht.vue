219e2a568e742f35277be8571bc9576a0b4d68c8
siggi <siegfried.hamm@gmail.com>
2022-02-20 17:12:17 +0100

    new div isPublished

--- src/components/Spielbericht.vue
+++ src/components/Spielbericht.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
+    <div>
     <h2>Spielbericht</h2>
     <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
+    </div>
+    <div>
+    <h2>Wurde dieser Spielbericht veröffentlicht?</h2>
+    <h3>{{ isPublished ? "Ja" : "Nein" }}</h3>
+    </div>
   </div>
 </template>
 <script>
