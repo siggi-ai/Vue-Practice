55ed202dcd1b04f3209d0168bcf3ef4344cafb37
siggi <siegfried.hamm@gmail.com>
2022-02-20 17:08:47 +0100

    data type required

--- src/components/Spielbericht.vue
+++ src/components/Spielbericht.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <h2>Spielbericht</h2>
-        <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
-    </div>
+  <div>
+    <h2>Spielbericht</h2>
+    <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
+  </div>
 </template>
 <script>
 export default {
     name: "Spielbericht",
     props: {
-        homeTeam {},
-        guestTeam {},
-    }
+        homeTeam {
+            type: String,
+            required: true,
+        },
+        guestTeam {
+            type: String,
+            required: true
+        },
+    };
 }
 </script>
