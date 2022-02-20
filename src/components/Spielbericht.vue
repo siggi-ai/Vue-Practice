163a9c3de2bd69da8c65c67a5f82a27c774868b6
siggi <siegfried.hamm@gmail.com>
2022-02-20 17:14:43 +0100

    complete template with data

--- src/components/Spielbericht.vue
+++ src/components/Spielbericht.vue
@@ -1,29 +1,36 @@
 <template>
   <div>
     <div>
-    <h2>Spielbericht</h2>
-    <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
-    <h4>Zuschauer: {{ spectators }}</h4>
+      <h2>Spielbericht</h2>
+      <h3>{{ homeTeam }} gegen {{ guestTeam }}</h3>
+      <h4>Zuschauer: {{ spectators }}</h4>
     </div>
     <div>
-    <h2>Wurde dieser Spielbericht veröffentlicht?</h2>
-    <h3>{{ isPublished ? "Ja" : "Nein" }}</h3>
+      <h2>Wurde dieser Spielbericht veröffentlicht?</h2>
+      <h3>{{ isPublished ? "Ja" : "Nein" }}</h3>
     </div>
   </div>
 </template>
 <script>
 export default {
-    name: "Spielbericht",
-    props: {
-        homeTeam: {
-            type: String,
-            required: true,
-        },
-        guestTeam: {
-            type: String,
-            required: true
-        },
-        
-    };
+  name: "Spielbericht",
+  props: {
+    homeTeam: {
+      type: String,
+      required: true,
+    },
+    guestTeam: {
+      type: String,
+      required: true,
+    },
+    spectators: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
