af24311ad6e7ae08a9c423618d6b3f1f4877baae
siggi <siegfried.hamm@gmail.com>
2022-02-21 08:54:36 +0100

    root id

--- src/components/Spielbericht.vue
+++ src/components/Spielbericht.vue
@@ -1,6 +1,8 @@
 <template>
-  <h2>Spielbericht</h2>
-  <div v-bind="$attrs">child komponente ohne root-element</div>
+  <div>
+    <h2>Spielbericht</h2>
+    <div v-bind="$attrs">child komponente ohne root-element</div>
+  </div>
 </template>
 <script>
 export default {
