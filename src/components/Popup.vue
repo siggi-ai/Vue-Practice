f8dd8b52da0ca4f519ab509358ed78eb55c57a67
siggi <siegfried.hamm@gmail.com>
2022-02-23 09:01:12 +0100

    use emit

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <h2>Popup.vue</h2>
-    <h2>hier popt die Nachricht auf :)</h2>
+    <h2>hier poppt die Nachricht auf :)</h2>
+    <button v-on:click="$emit('close')">Popup schließen</button>
   </div>
 </template>
 <script>
