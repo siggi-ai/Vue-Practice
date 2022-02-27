5e906e8867ab6b4506608b0f0ff73ef025284697
siggi <siegfried.hamm@gmail.com>
2022-02-27 08:46:22 +0100

    CardSlot

---
+++ src/components/CardSlot.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>{{ title }}</div>
+    <div>{{ body }}</div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardSlot",
+  props: {
+    title: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+  },
+};
+</script>
+<style>
+a {
+  padding: 10px;
+}
+</style>
