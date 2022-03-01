d8aa849e58147718fa84497cfc0649b7049dee33
siggi <siegfried.hamm@gmail.com>
2022-03-01 09:46:25 +0100

    starting playerslist

--- src/components/PlayersList.vue
+++ src/components/PlayersList.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        
-    </div>
+  <div></div>
 </template>
+<script>
+export default {
+  name: "PlayersList",
+  data() {
+    return {
+      players: [
+        {
+          firstName: "Klaus",
+          lastName: "Fischer",
+          age: 21
+        },
+      ],
+    };
+  },
+};
+</script>
