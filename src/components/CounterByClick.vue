40aaf011f3d6bd2d5cc709c26fe5ff5f20dbc0b8
siggi <siegfried.hamm@gmail.com>
2022-04-06 12:15:52 +0200

    style button

--- src/components/CounterByClick.vue
+++ src/components/CounterByClick.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button v-on:click="addCount">{{ count }}-mal mit der Maus geklickt</button>
+    <button v-on:click="addCount" class="button">
+      {{ count }}-mal mit der Maus geklickt
+    </button>
   </div>
 </template>
 <script>
@@ -18,3 +20,12 @@ export default {
   },
 };
 </script>
+<style>
+.button {
+  background-color: aqua;
+  width: 120px;
+  height: 100px;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: 20px;
+}
+</style>
