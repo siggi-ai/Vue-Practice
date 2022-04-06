6c14fc4effe260e88ac1243bf046317ec027dab8
siggi <siegfried.hamm@gmail.com>
2022-04-06 12:09:06 +0200

    method and data

--- src/components/CounterByHover.vue
+++ src/components/CounterByHover.vue
@@ -1,8 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <h2 v-on:mouseover="addCount">{{ count }}-mal mit der Maus überfahren</h2>
+  </div>
 </template>
 <script>
 export default {
   name: "Counter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count += 1;
+    },
+  },
 };
 </script>
