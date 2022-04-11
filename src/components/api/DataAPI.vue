0eb99bd7cbd65f55d6db8e4b7917aace95a3d599
siggi <siegfried.hamm@gmail.com>
2022-04-11 14:58:53 +0200

    more data

--- src/components/api/DataAPI.vue
+++ src/components/api/DataAPI.vue
@@ -8,11 +8,15 @@ export default {
   name: "DataAPI",
   setup() {
     const firstName = ref("Klaus");
+    const lastName = ref("Müller");
+    const club = ref("Bayern München");
     firstName.value = "Angela";
-    const greeting = `Hallo ${firstName.value}`;
+    const greeting = `Hallo ${firstName.value} ${lastName.value} ($club.value)`;
     console.log("firstName: ", firstName);
     return {
       firstName,
+      lastName,
+      club,
       greeting,
     };
   },
