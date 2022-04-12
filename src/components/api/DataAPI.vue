e167286b5c850e731e1abd8bcd1c9cd96582a15c
siggi <siegfried.hamm@gmail.com>
2022-04-12 14:37:38 +0200

    introduction

--- src/components/api/DataAPI.vue
+++ src/components/api/DataAPI.vue
@@ -4,6 +4,7 @@
   <h3>{{ state.lastName }}</h3>
   <h3>{{ state.club }}</h3>
   <p>{{ greeting }}</p>
+  <p>{{ introduction }}</p>
 </template>
 <script>
 import { reactive } from "vue";
@@ -17,9 +18,11 @@ export default {
       hobby: "sailing",
     });
     const greeting = `Hallo ${state.firstName} ${state.lastName} (${state.club})`;
+    const introduction = `I am ${state.firstName} + ' ' + and I like ${state.hobby}`;
     return {
       state,
       greeting,
+      introduction,
     };
   },
 };
