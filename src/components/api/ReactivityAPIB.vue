8567e92f499a07cad465995076225a2462a857ac
siggi <siegfried.hamm@gmail.com>
2022-04-13 13:47:37 +0200

    new state

---
+++ src/components/api/ReactivityAPIB.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ state.firstName }} {{ state.lastName }}</h2>
+  </div>
+</template>
+<script>
+import { reactive } from "vue";
+export default {
+  name: "ReactivityAPIA",
+  setup() {
+    const state = reactive({
+      firstName: "Angela",
+      lastName: "Merkel",
+    });
+    return {
+      state,
+    };
+  },
+};
+</script>
