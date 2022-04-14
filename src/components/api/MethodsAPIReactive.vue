972c3b2f124cb5d3f2fdb937139275c9000ea2e3
siggi <siegfried.hamm@gmail.com>
2022-04-14 10:32:47 +0200

    MethodsAPIReactive

--- src/components/api/MethodsAPIReactive.vue
+++ src/components/api/MethodsAPIReactive.vue
@@ -1,8 +1,35 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Neue Schreibweise</h2>
+    <h3>{{ firstName }} {{ lastName }}</h3>
+    <h4>{{ goals }}</h4>
+    <button v-on:click.prevent="changeName">Namen ändern</button>
+    <button v-on:click.prevent="newGoal">Tor erzielt</button>
+  </div>
 </template>
 <script>
+import { ref } from "vue";
 export default {
-    name: "",
+  name: "MethodsAPIRef",
+  setup() {
+    const firstName = ref("Gerd");
+    const lastName = ref("Hamm");
+    const goals = ref(0);
+
+    function changeName() {
+      this.firstName = "Siegfried";
+      this.lastName = "Choi";
+    }
+    function newGoal() {
+      goals.value++;
+    }
+    return {
+      firstName,
+      lastName,
+      goals,
+      changeName,
+      newGoal,
+    };
+  },
 };
 </script>
