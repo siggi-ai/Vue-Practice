c80e22fd5aac12ab5c0f9ddf1cb9fdb7fdfc72c2
siggi <siegfried.hamm@gmail.com>
2022-04-14 10:12:46 +0200

    methods ready

--- src/components/api/MethodsAPIRef.vue
+++ src/components/api/MethodsAPIRef.vue
@@ -1,8 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <h2>Alte Schreibweise</h2>
+    <h3>{{ firstName }} {{ lastName }}</h3>
+    <h4>{{ goals }}</h4>
+    <button v-on:click.prevent="changeName">Namen ändern</button>
+    <button v-on:click.prevent="newGoal">Tor erzielt</button>
+  </div>
 </template>
 <script>
 export default {
-    name: "",
+  name: "Methods",
+  data() {
+    return {
+      firstName: "Gerd",
+      lastName: "Hamm",
+      goals: 0,
+    };
+  },
+  methods: {
+    changeName() {
+      this.firstName = "Siegfried";
+    },
+    newGoal() {
+      this.goals++;
+    },
+  },
 };
 </script>
