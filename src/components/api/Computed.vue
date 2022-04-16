b8020828529d24b8d7e7b07ea86853718dde29f9
siggi <siegfried.hamm@gmail.com>
2022-04-16 17:38:51 +0200

    computed.vue

---
+++ src/components/api/Computed.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label for="firstName">
+      <span>Vorname</span>
+    </label>
+    <input
+      type="text"
+      id="firstName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <label for="lastName">
+      <span>Nachname</span>
+    </label>
+    <input
+      type="text"
+      id="lastName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <h2>{{ fullName }}</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Computed",
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+    };
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  },
+};
+</script>
