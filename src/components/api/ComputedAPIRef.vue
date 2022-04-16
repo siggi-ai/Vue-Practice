eb3f4d99e400b86bd297e47a5638e05173259b94
siggi <siegfried.hamm@gmail.com>
2022-04-16 17:55:33 +0200

    computed setup

---
+++ src/components/api/ComputedAPIRef.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label for="firstName">
+      <span>Vorname</span>
+    </label>
+    <input
+      type="text"
+      id="firstName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <label for="lastName">
+      <span>Nachname</span>
+    </label>
+    <input
+      type="text"
+      id="lastName"
+      v-model="lastName"
+      placeholder="Nachname"
+    />
+    <h2>Hello {{ fullName }} !</h2>
+  </div>
+</template>
+<script>
+import { ref, computed } from "vue";
+export default {
+  name: "ComputedAPIRef",
+  setup() {
+    const firstName = ref("");
+    const lastName = ref("");
+    //
+    const fullName = computed(function () {
+      return `${firstName.value} ${lastName.value}`;
+    });
+    //
+    return {
+      firstName,
+      lastName,
+      fullName,
+    };
+  },
+};
+</script>
