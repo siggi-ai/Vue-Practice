1d16f02d2146a3ef0bb9f522d5a133bddf4d718f
siggi <siegfried.hamm@gmail.com>
2022-04-16 17:39:54 +0200

    computed fullName

--- src/components/api/Computed.vue
+++ src/components/api/Computed.vue
@@ -15,8 +15,8 @@
     <input
       type="text"
       id="lastName"
-      v-model="firstName"
-      placeholder="Vorname"
+      v-model="lastName"
+      placeholder="Nachname"
     />
     <h2>{{ fullName }}</h2>
   </div>
