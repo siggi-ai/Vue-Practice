ed6920996959ad12df3d6def1a7385ad18a2ae6c
siggi <siegfried.hamm@gmail.com>
2022-04-16 17:23:55 +0200

    branch is up to date with 'origin/master'.

--- src/components/api/VModelAPIRef.vue
+++ src/components/api/VModelAPIRef.vue
@@ -1,8 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <label for="firstName">
+      <span>Vorname</span>
+    </label>
+    <input
+      type="text"
+      id="firstName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <label for="lastName">
+      <span>Nachname</span>
+    </label>
+    <input
+      type="text"
+      id="lastName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+  </div>
 </template>
 <script>
+import { ref } from "vue";
 export default {
   name: "VModelAPIRef",
+  setup() {
+    const firstName = ref("");
+    const lastName = ref("");
+    return {
+      firstName,
+      lastName,
+    };
+  },
 };
 </script>
