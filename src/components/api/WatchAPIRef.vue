9ec269c846360626f84b430af7cd375046f0531b
siggi <siegfried.hamm@gmail.com>
2022-04-19 10:04:15 +0200

    2nd template

---
+++ src/components/api/WatchAPIRef.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label for="firstName">
+      <span>Vorname</span>
+    </label>
+    <input
+      type="text"
+      id="firstName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <input
+      type="text"
+      id="lastName"
+      v-model="lastName"
+      placeholder="Nachname"
+    />
+  </div>
+</template>
+<script>
+import { ref, watch } from "vue";
+export default {
+  name: "WatchAPIRef",
+  //
+  setup() {
+      const firstName = ref("Klaus");
+      const lastName = ref("Fischer");
+
+  watch(firstName, (newValue, oldValue) => {
+      console.log("Watch old value: ", oldValue);
+      console.log("Watch new value: ", newValue);
+    },
+    lastName(newValue, oldValue) {
+      console.log("Watch old value: ", oldValue);
+      console.log("Watch new value: ", newValue);
+    }),
+  },
+};
+</script>
