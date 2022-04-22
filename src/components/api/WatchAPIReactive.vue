d4668ce66e3b2b59692d604c53c9498341ca4ad5
siggi <siegfried.hamm@gmail.com>
2022-04-22 10:24:19 +0200

    WatchAPIReactive

---
+++ src/components/api/WatchAPIReactive.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <label for="firstName">
+      <span>Vorname</span>
+    </label>
+    <input
+      type="text"
+      id="firstName"
+      v-model="firstName"
+      placeholder="Vorname"
+    />
+    <input
+      type="text"
+      id="lastName"
+      v-model="lastName"
+      placeholder="Nachname"
+    />
+  </div>
+</template>
+<script>
+import { reactive, toRefs, watch } from "vue";
+export default {
+  name: "WatchAPIReactive",
+  //
+  setup() {
+    const state = reactive({
+      firstName: "Uwe",
+      lastName: "Seeler",
+    });
+    watch(
+      state,
+      function (newValue, oldValue) {
+        console.log("WatchAPIReactive old value: ", oldValue);
+        console.log("WatchAPIReactive new value: ", newValue);
+      },
+      {
+        immediate: true,
+      }
+    );
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
