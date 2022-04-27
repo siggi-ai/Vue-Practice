e2df60a3d802b5698a4dae29d5b18c48c1a055f7
siggi <siegfried.hamm@gmail.com>
2022-04-27 22:34:40 +0200

    framework provide

---
+++ src/components/ProvideInject.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>ProvideInject: {{ framework }}</h3>
+    <KidA />
+  </div>
+</template>
+<script>
+import KidA from "./KidA.vue";
+export default {
+  name: "ProvideInject",
+  //
+  components: {
+    KidA,
+  },
+  data() {
+    return {
+      framework: "Vue3",
+    };
+  },
+  provide() {
+    return {
+      frameworkName: this.framework,
+    };
+  },
+};
+</script>
