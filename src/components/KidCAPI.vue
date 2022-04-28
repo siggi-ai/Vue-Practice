997f7dcb87bd05e8bb6465c76260ad92a05bf762
siggi <siegfried.hamm@gmail.com>
2022-04-28 15:25:07 +0200

    PI-API

---
+++ src/components/KidCAPI.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h4>Kid C</h4>
+    <div>Name of the framework: {{ frameworkNameAPI }}</div>
+  </div>
+</template>
+<script>
+import { inject } from "vue";
+export default {
+  name: "KidCAPI",
+  //
+  setup() {
+    const frameworkNameAPI = inject("framworkNameAPI", "Default framework");
+    //
+    return {
+      frameworkNameAPI,
+    };
+  },
+};
+</script>
