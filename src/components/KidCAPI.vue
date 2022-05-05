488e65e87fca8248c19c1c6e2ce969a35893994d
siggi <siegfried.hamm@gmail.com>
2022-05-05 15:57:43 +0200

    child speaks

--- src/components/KidCAPI.vue
+++ src/components/KidCAPI.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
     <h4>Kid C</h4>
-    <div>Name of the framework: {{ frameworkNameAPI }}</div>
+    <p>Spieler: {{ firstName }} {{ lastName }}</p>
   </div>
-  <h1>{{ message }}</h1>
 </template>
 <script>
-import { inject } from "vue";
+import { inject, toRefs } from "vue";
 export default {
   name: "KidCAPI",
   //
   setup() {
-    const frameworkNameAPI = inject("frameworkNameAPI", "Default framework");
+    const childGoals = inject("c_goals", 0);
+    const childPlayer = inject("c_player", {
+      firstName: "Klaus",
+      lastName: "Fischer",
+    });
     //
     return {
-      frameworkNameAPI,
+      childGoals,
+      ...toRefs(childPlayer),
     };
-  },
-  inject: ["message"],
-  created() {
-    console.log(this.message); // injected value
   },
 };
 </script>
